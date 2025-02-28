c13a90e68820f886d530c72b6eafebdeceb597b1
VitorRosa11 <vitorrb1103@gmail.com>
2025-02-28 13:48:06 -0300

    Add files via upload

---
+++ Privado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package projeto2.privado;
+
+/**
+ *
+ * @author vinic
+ */
+public class Privado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
